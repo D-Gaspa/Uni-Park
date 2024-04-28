36352ad24bb9f363a2effd6a01b7e5f852a76ea3
Diego Gasparis Escobedo <diego.gaspariseo@udlap.mx>
2024-04-28 12:49:53 -0600

    Finish parking data db merge

--- components/parkingSpots.ts
+++ components/parkingSpots.ts
@@ -1,35 +1,35 @@
-import { ref, onValue } from "firebase/database";
-import { db } from '@/app/_layout'
+import {ref, onValue} from "firebase/database";
+import {db} from '@/app/_layout'
 
 interface Coordinate {
-  latitude: number;
-  longitude: number;
+    latitude: number;
+    longitude: number;
 }
 
 export interface ParkingSpot {
-  id: string;
-  coordinates: Coordinate[];
-  totalSpots: number;
-  availableSpots: number;
-  selected?: boolean; // Local property
+    id: string;
+    coordinates: Coordinate[];
+    totalSpots: number;
+    availableSpots: number;
+    selected?: boolean;
 }
 
 export function getParkingSpots(
-  setParkingSpots: (spots: ParkingSpot[]) => void,
+    setParkingSpots: (spots: ParkingSpot[]) => void,
 ): void {
-  const spotsRef = ref(db,'/spots/');
-  onValue(spotsRef, snapshot => {
-    const spotsArray: ParkingSpot[] = [];
-    const data = snapshot.val();
-    for (const id in data) {
-      const spot = data[id];
-      spotsArray.push({
-        id,
-        coordinates: spot.coordinates,
-        totalSpots: spot.totalSpots,
-        availableSpots: spot.availableSpots,
-      });
-    }
-    setParkingSpots(spotsArray);
-  });
+    const spotsRef = ref(db, '/spots/');
+    onValue(spotsRef, snapshot => {
+        const spotsArray: ParkingSpot[] = [];
+        const data = snapshot.val();
+        for (const id in data) {
+            const spot = data[id];
+            spotsArray.push({
+                id: spot.id,
+                coordinates: spot.coordinates,
+                totalSpots: spot.totalSpots,
+                availableSpots: spot.availableSpots,
+            });
+        }
+        setParkingSpots(spotsArray);
+    });
 }
