06711f92ab07fe59542b78f62665a8afd15051c8
Diego Gasparis Escobedo <diego.gaspariseo@udlap.mx>
2024-04-28 12:49:53 -0600

    Finish parking data db merge

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,17 @@
-import { Slot } from "expo-router";
-import { SessionProvider } from "@/components/AuthContext.tsx";
-import { useColorScheme } from "@/components/useColorScheme";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import {Slot} from "expo-router";
+import {SessionProvider} from "@/components/AuthContext.tsx";
+import {useColorScheme} from "@/components/useColorScheme";
+import {DarkTheme, DefaultTheme, ThemeProvider} from "@react-navigation/native";
+import {initializeAuth, getReactNativePersistence} from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { firebaseConfig } from "@/firebaseConfig";
-import { initializeApp } from "firebase/app";
-import { Platform } from "react-native";
-import { getDatabase } from "firebase/database";
+import {firebaseConfig} from "@/firebaseConfig";
+import {initializeApp} from "firebase/app";
+import {Platform} from "react-native";
+import {getDatabase} from "firebase/database";
 
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+    // Catch any errors thrown by the Layout component.
+    ErrorBoundary,
 } from "expo-router";
 
 // Initialize Firebase
@@ -23,23 +19,23 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Auth with AsyncStorage for Android and iOS
 if (Platform.OS !== "web") {
-  initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage),
-  });
+    initializeAuth(app, {
+        persistence: getReactNativePersistence(AsyncStorage),
+    });
 }
 
 const db = getDatabase(app);
 
-export { db };
+export {db};
 
 export default function Root() {
-  return (
-    <ThemeProvider
-      value={useColorScheme() === "dark" ? DarkTheme : DefaultTheme}
-    >
-      <SessionProvider>
-        <Slot />
-      </SessionProvider>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider
+            value={useColorScheme() === "dark" ? DarkTheme : DefaultTheme}
+        >
+            <SessionProvider>
+                <Slot/>
+            </SessionProvider>
+        </ThemeProvider>
+    );
 }
