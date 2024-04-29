87c03c27356ba57c0b3eb9fddd4e59e9081753cf
Georgina Zeron <93169706+930r91na@users.noreply.github.com>
2024-04-29 08:30:32 -0600

    Server and pay layout

--- app/(app)/(tabs)/ticket.tsx
+++ app/(app)/(tabs)/ticket.tsx
@@ -1,31 +1,22 @@
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, Text } from 'react-native';
+import Payment from '../payment'; // Import the FaqModal component
+import { Link } from 'expo-router';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-
-export default function TicketScreen() {
+const TicketScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/ticket.tsx"/>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Ticket Information</Text>
+      <Link href="/payment" asChild>
+        <Button
+          title='Pay Now'
+          accessibilityLabel='Pay Now'
+
+        >
+        </Button>
+      </Link>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+export default TicketScreen;
