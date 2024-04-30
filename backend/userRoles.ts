4ac4faa6097b2437bdd52a6d844df5fe737aeb03
Georgina Zeron <93169706+930r91na@users.noreply.github.com>
2024-04-30 10:39:47 -0600

    Ticket so far, (getting qr codes generated)

--- backend/userRoles.ts
+++ backend/userRoles.ts
@@ -12,3 +12,21 @@ export async function getUserRole(user: string) {
         return null;
     }
 }
+
+export async function getUserTickets(user: string): Promise<string[] | null> {
+    const docRef = doc(userRoles, "user-roles", user);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        const list_tickets = docSnap.data().list_tickets;
+        if (Array.isArray(list_tickets) && list_tickets.every(item => typeof item === 'string')) {
+            return list_tickets; 
+        } else {
+            console.log("list_tickets is not an array of strings!");
+            return null;
+        }
+    } else {
+        console.log("No such document!");
+        return null;
+    }
+}
