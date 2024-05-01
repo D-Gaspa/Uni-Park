3669b42408089e461727c6026d6918aa93ec7212
Diego Gasparis Escobedo <diego.gaspariseo@udlap.mx>
2024-05-01 14:38:50 -0600

    Refactor Firebase initialization and configuration. Fix Firebase not being initialized before being used.
    Firebase initialization and configuration has been moved from a separate file to the firebase-config file. The firebase configurations were also updated in other files using them. This makes our code cleaner and easier to manage, maintaining all our Firebase setups in one place. Additionally, small changes were also made, such as renaming color constants, and refining comments.

--- components/useColorScheme.web.ts
+++ components/useColorScheme.web.ts
@@ -2,7 +2,7 @@
 // Server rendered styles should not change between the first render of the HTML
 // and the first render on the client. Typically, web developers will use CSS media queries
 // to render different styles on the client and server, these aren't directly supported in React Native
-// but can be achieved using a styling library like Nativewind.
+// but can be achieved using a styling library like Native wind.
 export function useColorScheme() {
   return 'dark';
 }
--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,32 @@
-// Add the Firebase products that you want to use
-// import {...} from "firebase/database";
-// import {...} from "firebase/firestore";
-// import {...} from "firebase/functions";
-// import {...} from "firebase/storage";
+import { getApps, initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Platform } from "react-native";
 
-// Initialize Firebase
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyBk0FYGZ1a3lSpul6CmF2CFFl7Bhp3qoWE",
   authDomain: "uni-park-9ac04.firebaseapp.com",
   projectId: "uni-park-9ac04",
   storageBucket: "uni-park-9ac04.appspot.com",
   messagingSenderId: "255781547195",
-  appId: "1:255781547195:web:8179da3dc2fc7808c38d9e",
+  appId: "1:255781547195:web:8179da3dc2fc2fc7808c38d9e",
   measurementId: "G-BMKFLX250S"
 };
+
+let app;
+if (!getApps().length) {
+    app = initializeApp(firebaseConfig);
+    if (Platform.OS !== "web") {
+        initializeAuth(app, {
+            persistence: getReactNativePersistence(AsyncStorage),
+        });
+    }
+} else {
+    app = getApps()[0];
+}
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+export const userRoles = getFirestore(app);
