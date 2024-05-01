fdf4ee69dd35c120a5fea987b0de0a9d02991138
Diego Gasparis Escobedo <diego.gaspariseo@udlap.mx>
2024-05-01 17:46:57 -0600

    Refactor report screen layout and add dark mode support
    The layout of the report screen was refactored to improve code readability. Additionally, support for dark mode was added. The placeholder text color adapts to the current color scheme, and the dropdown menu's border color has also been updated to fit the dark mode. Finally, the checkbox component was added to the project dependencies.

--- components/reportInteraction.ts
+++ components/reportInteraction.ts
@@ -1,27 +1,18 @@
-import { ref, push } from "firebase/database";
-import { db } from "@/firebase-config";
-import { FormState } from "@/app/(app)/(tabs)/report";
-
-const reportTypes = [
-  "Violation Reports",
-  "Maintenance Issues",
-  "Safety Issues",
-  "Suggestions for Improvement",
-  "Feedback on Parking Enforcement",
-] as const;
-
+import {push, ref} from "firebase/database";
+import {db} from "@/firebase-config";
+import {FormState} from "@/app/(app)/(tabs)/report";
 
 export const sendReport = async (form: FormState, email: string) => {
-  if (
-    !form.title ||
-    !form.description ||
-    form.title.trim() === "" ||
-    form.description.trim() === ""
-  ) {
-    throw new Error("Please fill in the title and description fields.");
-  }
-  form.email = email;
-  form.date = new Date().toISOString();
+    if (
+        !form.title ||
+        !form.description ||
+        form.title.trim() === "" ||
+        form.description.trim() === ""
+    ) {
+        throw new Error("Please fill in the title and description fields.");
+    }
+    form.email = email;
+    form.date = new Date().toISOString();
 
-  await push(ref(db, "/reports"), form);
+    await push(ref(db, "/reports"), form);
 };
